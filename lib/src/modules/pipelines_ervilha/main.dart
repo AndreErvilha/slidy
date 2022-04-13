8eb067a8e85b3a8da88cf4115c4ba01531363d5b
andreervilha <andre.ervilha@cwi.com.br>
2022-04-12 23:48:45 -0300

    join feat markdown with pipeline

--- lib/src/modules/pipelines_ervilha/main.dart
+++ lib/src/modules/pipelines_ervilha/main.dart
@@ -1,19 +1,15 @@
 import 'dart:io';
 
-import 'package:slidy/src/modules/pipelines_ervilha/modules/markdown/markdown.dart';
-
 import 'modules/core/commands/capture.dart';
 import 'modules/pipeline/pipeline.dart';
 
 main() {
-  final markdown = Markdown()
-    ..loadMarkdown(File(
-        '/Users/andreervilha/StudioProjects/slidy/lib/src/modules/pipelines_ervilha/modules/markdown/pipe.md'));
-
-  Pipeline()
+  final pipe = Pipeline()
     ..addCommand(Capture())
-    ..loadYaml(File(
-        '/Users/andreervilha/StudioProjects/slidy/lib/src/modules/pipelines_ervilha/pipe.yaml'));
+    ..loadMarkdown(File(
+        '/Users/andreervilha/StudioProjects/slidy/lib/src/modules/pipelines_ervilha/pipe.md'));
+  // ..loadYaml(File(
+  //     '/Users/andreervilha/StudioProjects/slidy/lib/src/modules/pipelines_ervilha/pipe.yaml'));
 
   print('');
 }
